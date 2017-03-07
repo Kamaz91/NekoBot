6f06befeb4db714f7ba5fc77fa369677417ddcad
Kamil Świątek <Kamaz91@gmail.com>
2017-03-07 17:46:30 +0100

    - Zmiana na CONFIG

--- triggers/setgame.js
+++ triggers/setgame.js
@@ -1,8 +1,8 @@
-/* global ADMIN_ID */
+/* global ADMIN_ID, CONFIG */
 
 var method = setgame.prototype;
 function setgame(message, trigger) {
-    if (message.author.id === ADMIN_ID) {
+    if (message.author.id === CONFIG.AdminId) {
         if (trigger.text.length > 1) {
             client.user.setGame(trigger.text);
         } else {
