ab263515acee24641c331cf8f200e17e833e5ecc
Kamil Świątek <Kamaz91@gmail.com>
2017-03-11 02:59:32 +0100

    - Połączono ReloadModule z Triggers - Dodano przeładowywanie wyzwalaczy komendą !reload

--- includes/Trigger.js
+++ includes/Trigger.js
@@ -1,4 +1,4 @@
-/* global ADMIN_ID, WOT_APP_ID, FS */
+/* global ADMIN_ID, WOT_APP_ID, FS, CONFIG */
 
 var method = Trigger.prototype;
 function Trigger() {
@@ -43,25 +43,79 @@ method.loadTriggers = function () {
 };
 
 method.reloadTriggers = function () {
+    for (var trigger of this.triggersList) {
+        this.removeTrigger(trigger.path);
+    }
+    this.loadTriggers();
+};
 
+method.removeTrigger = function (moduleName) {
+    var purgeCache = function (moduleName) {
+        // Traverse the cache looking for the files
+        // loaded by the specified module name
+        searchCache(moduleName, function (mod) {
+            delete require.cache[mod.id];
+        });
+
+        // Remove cached paths to the module.
+        // Thanks to @bentael for pointing this out.
+        Object.keys(module.constructor._pathCache).forEach(function (cacheKey) {
+            if (cacheKey.indexOf(moduleName) > 0) {
+                delete module.constructor._pathCache[cacheKey];
+            }
+        });
+    };
+
+    /**
+     * Traverses the cache to search for all the cached
+     * files of the specified module name
+     */
+    var searchCache = function (moduleName, callback) {
+        // Resolve the module identified by the specified name
+        var mod = require.resolve(moduleName);
+
+        // Check if the module has been resolved and found within
+        // the cache
+        if (mod && ((mod = require.cache[mod]) !== undefined)) {
+            // Recursively go over the results
+            (function traverse(mod) {
+                // Go over each of the module's children and
+                // traverse them
+                mod.children.forEach(function (child) {
+                    traverse(child);
+                });
+
+                // Call the specified callback providing the
+                // found cached module
+                callback(mod);
+            }(mod));
+        }
+    };
+    purgeCache(moduleName);
 };
 
 method.checkTrigger = function (message) {
-    /* Objekt wiadomości */
-    /* Obiekt trigger */
-    var params = {};
-    /* Rozdzielenie wiadomości po spacjach */
-    params['splitTigger'] = message.content.split(' ');
-    /* Trigger */
-    params['trigger'] = params.splitTigger[0];
-    /* Tekst wiadomości bez triggera */
-    params['text'] = message.content.slice(params.splitTigger[0].length).trim();
 
-    if (this.triggers[params.trigger]) {
-        try {
-            this.triggers[params.trigger](message, params);
-        } catch (exception) {
-            console.log(exception);
+    if (!message.author.bot) {
+        /* Objekt wiadomości */
+        /* Obiekt trigger */
+        var params = {};
+        /* Rozdzielenie wiadomości po spacjach */
+        params['splitTigger'] = message.content.split(' ');
+        /* Trigger */
+        params['trigger'] = params.splitTigger[0];
+        /* Tekst wiadomości bez triggera */
+        params['text'] = message.content.slice(params.splitTigger[0].length).trim();
+
+        if (params['trigger'] === '!reload' && message.author.id === CONFIG.AdminId) {
+            this.reloadTriggers();
+        } else
+        if (this.triggers[params.trigger]) {
+            try {
+                this.triggers[params.trigger](message, params);
+            } catch (exception) {
+                console.log(exception);
+            }
         }
     }
 };
