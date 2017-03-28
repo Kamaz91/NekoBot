e88b6eda16dfbf8462b042ecce6b03bc722a182f
Kamil Świątek <Kamaz91@gmail.com>
2017-03-28 03:35:46 +0200

    - zmiana z textu na embed

--- triggers/wot.js
+++ triggers/wot.js
@@ -24,28 +24,39 @@ function wot(message, trigger) {
 
                         var ll = new Date(body.data[profile.account_id].logout_at * 1000);
                         var lb = new Date(body.data[profile.account_id].last_battle_time * 1000);
-                        console.log(body.data[profile.account_id].last_battle_time);
-                        all = body.data[profile.account_id].statistics.all;
-                        string = '```' +
-                                `Id          : ${profile.account_id} \n` +
-                                `Nick        : ${profile.nickname} \n` +
-                                `Exp         : ${all.xp}\n` +
-                                `--------------------------------\n` +
-                                `Battles     : ${all.battles}\n` +
-                                `Win         : ${all.wins}\n` +
-                                `Losses      : ${all.losses}\n` +
-                                `Draws       : ${all.draws}\n` +
-                                `Win ratio   : ${((all.wins / all.battles) * 100).toString().slice(0, 5)}% \n` +
-                                `Survived    : ${all.survived_battles}\n` +
-                                `--------------------------------\n` +
-                                `Frags       : ${all.frags}\n` +
-                                `Max damage  : ${all.max_damage}\n` +
-                                `Last online : ${('0' + ll.getDate()).slice(-2)} / ${('0' + (ll.getMonth() + 1)).slice(-2)} / ${ll.getFullYear()} ` +
-                                `${ll.toLocaleTimeString()}\n` +
-                                `Last battle : ${('0' + lb.getDate()).slice(-2)} / ${('0' + (lb.getMonth() + 1)).slice(-2)} / ${lb.getFullYear()} ` +
-                                `${lb.toLocaleTimeString()}\n` +
-                                '```';
-                        message.channel.sendMessage(string);
+                        var data = body.data[profile.account_id];
+                        var all = body.data[profile.account_id].statistics.all;
+                        const embed = new Discord.RichEmbed()
+                                .setAuthor(profile.nickname, 'https://www.spreadshirt.pl/image-server/v1/designs/16507080,width=178,height=178/world-of-tanks-logo.png')
+                                .setColor(0xEF017C)
+                                .setFooter('Kraj Pochodzenia: ' + data.client_language.toString().toUpperCase(), `http://flags.fmcdn.net/data/flags/h80/${data.client_language}.png`)
+                                .setImage(`http://wotlabs.net/sig_dark/eu/${profile.nickname}/signature.png`)
+                                .setURL('http://worldoftanks.eu/pl/community/accounts/' + profile.account_id)
+
+                                .addField('Basic',
+                                        `Id          : ${profile.account_id} \n` +
+                                        `Nick        : ${profile.nickname} \n` +
+                                        `Exp         : ${all.xp}`)
+                                .addField('Battles',
+                                        `Battles     : ${all.battles}\n` +
+                                        `Win         : ${all.wins}\n` +
+                                        `Losses      : ${all.losses}\n` +
+                                        `Draws       : ${all.draws}\n` +
+                                        `Survived    : ${all.survived_battles}\n` +
+                                        `Win ratio   : ${((all.wins / all.battles) * 100).toString().slice(0, 5)} % \n` +
+                                        `Surv ratio  : ${((all.survived_battles / all.battles) * 100).toString().slice(0, 5)} % `)
+                                .addField('Damage',
+                                        `Frags       : ${all.frags}\n` +
+                                        `Max damage  : ${all.max_damage}`)
+                                .addField('Last',
+                                        `Last online : ${('0' + ll.getDate()).slice(-2)} / ${('0' + (ll.getMonth() + 1)).slice(-2)} / ${ll.getFullYear()} ` +
+                                        `${ll.toLocaleTimeString()}\n` +
+                                        `Last battle : ${('0' + lb.getDate()).slice(-2)} / ${('0' + (lb.getMonth() + 1)).slice(-2)} / ${lb.getFullYear()} ` +
+                                        `${lb.toLocaleTimeString()}`);
+                        if (data.clan_id !== null) {
+                            embed.setThumbnail(`http://eu.wargaming.net/clans/media/clans/emblems/cl_${(data.clan_id).toString().slice(-3)}/${data.clan_id}/emblem_195x195.png`);
+                        }
+                        message.channel.sendEmbed(embed);
                     }
                 });
             } else {
