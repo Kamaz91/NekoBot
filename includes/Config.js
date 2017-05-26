513fb9b14445b5f5ad845c8cf673c3623ebe11fa
Kamil Świątek <Kamaz91@gmail.com>
2017-05-26 16:56:19 +0200

    Nowy moduł Config

---
+++ includes/Config.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+class Config {
+    constructor() {
+        /* Załadowanie konfiguracji */
+        this.cfg = JSON.parse(fs.readFileSync('./config/config.json', 'utf8'));
+        this.adminId = this.cfg.AdminId;
+        this.logsChannelId = this.cfg.LogsChannelId;
+        /* Załadowanie tokenów */
+        this.tokens = JSON.parse(fs.readFileSync('./config/tokens.json', 'utf8'));
+    }
+
+    getToken(searchToken) {
+        return this.tokens[searchToken];
+    }
+}
+
+module.exports = Config;
