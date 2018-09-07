7d15cf263abc52ba49f03f2c24bb0e84379e2a59
Kamil Świątek <Kamaz91@gmail.com>
2018-09-07 20:41:14 +0200

    Dodany ModulesLoader (Nie testowane)

---
+++ modules/BasicTriggers/BasicTriggers.js
@@ -0,0 +1,9 @@
+class basicTrigs {
+    constructor(DiscordClient, triggerManager) {
+        triggerManager.addTrigger({
+            name: "xx",
+            content: "lol",
+            prefix: "!" //optional. if not defined using default prefix
+        });
+    }
+}
