196e7e5cf2f56ec01314649da2bb4757fd461e3c
Kamil Świątek <Kamaz91@gmail.com>
2018-09-17 20:37:20 +0200

    Module Loader działa poprawnie

--- includes/ModulesLoader.js
+++ includes/ModulesLoader.js
@@ -1,5 +1,7 @@
-const modulesDir = './modules/';
 const fs = require('fs');
+const path = require('path');
+
+const modulesDir = `${path.sep}modules${path.sep}`;
 
 class ModulesLoader {
 
@@ -7,32 +9,58 @@ class ModulesLoader {
         this.DiscordClient = DiscordClient;
         this.TriggerManager = TriggerManager;
         this.ModulesList = {};
+        this.loadModules();
     }
     loadModules() {
-        var ModuleObj = {};
-        fs.readdirSync(modulesDir).forEach(moduleName => {
+        console.log("Module Loader:");
+        fs.readdirSync(`.${modulesDir}`).forEach(moduleName => {
             // Przeszukiwanie folderu modułów
-            var jsonPath = modulesDir + moduleName + '/config.json';
+            var jsonPath = `.${modulesDir}${moduleName}${path.sep}config.json`;
             if (fs.existsSync(jsonPath)) {
-                var Config = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
+                console.log("Moduł: " + moduleName);
+                let jsonConfig = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
                 // Ładowanie konfiguracji modułu
-                var date = new Date();
-                var messageTime = ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2);
+                let date = new Date();
+                let t = [
+                    `0${date.getHours()}`.slice(-2),   // Godziny
+                    `0${date.getMinutes()}`.slice(-2), // Minuty
+                    `0${date.getSeconds()}`.slice(-2)  // Sekundy
+                ];
+                var messageTime = t.join(':');
                 // Czas systemu hh:mm:ss
-                ModuleObj = Config
-                ModuleObj.path = modulesDir + moduleName;
-                this.ModulesList[moduleName] = ModuleObj;
+                var ModuleObj = {
+                    name: moduleName,
+                    path: `..${modulesDir}${moduleName}${path.sep}`,
+                    module: null,
+                    enabled: false,
+                    status: false,
+                    start: null
+                };
+                // Szablon konfiguracji moodułu
+                ModuleObj = Object.assign(ModuleObj, jsonConfig);
+                // Dołączenie danych z pliku Json
 
-                if (moduleOpts.status) {
+                if (ModuleObj.enabled) {
+                    console.log(messageTime + ' Ładowanie modułu: ' + ModuleObj.name);
                     try {
-                        console.log(messageTime + ' Ładowanie modułu: ' + ModuleObj.name);
-                        let mod = require(modulesDir + moduleName + "/" + moduleName + ".js");
-                        ModuleObj.module = new mod(this.DiscordClient, this.TriggerManager);
-                        console.log('   - Załadowano moduł');
+                        var mod = require(`..${modulesDir}${moduleName}${path.sep}${ModuleObj.start}`);
                     } catch (exception) {
-                        console.log('   - Wystąpił błąd Podczas ładowania ');
+                        console.log('ERROR - Wystąpił błąd Podczas ładowania ');
                         console.log(exception);
                     }
+                    try {
+                        ModuleObj.module = new mod(this.DiscordClient, this.TriggerManager);
+                        // Inicjalizacja
+                        ModuleObj.status = true;
+                        // Zmiana statusu
+                        this.ModulesList[moduleName] = ModuleObj;
+                        // Dodawanie do listy
+
+                    } catch (exception) {
+                        console.log('ERROR - Wystąpił błąd Podczas inicjalizacji modułu ');
+                        console.log(exception);
+                    }
+                    console.log('------ Załadowano moduł ------');
                 } else {
                     console.log(messageTime + ' Pominięto moduł: ' + ModuleObj.name);
                 }
