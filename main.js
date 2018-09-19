23ca1b5aadf831e2ead37a8d50de2f09e2dfa6fa
Kamil Świątek <Kamaz91@gmail.com>
2018-09-19 16:00:26 +0200

    Modules Loader inicjalization
    voice listener przeniesiony do osobnego modułu

--- main.js
+++ main.js
@@ -17,11 +17,13 @@ knex = require('knex')({
 });
 
 function time() {
-    var date = new Date();
-    return ('0' + date.getHours()).slice(-2) + ':' +
-            ('0' + date.getMinutes()).slice(-2) + ':' +
-            ('0' + date.getSeconds()).slice(-2) + '.' +
-            ('00' + date.getMilliseconds()).slice(-3);
+    let date = new Date();
+    let t = [
+        `0${date.getHours()}`.slice(-2),   // Godziny
+        `0${date.getMinutes()}`.slice(-2), // Minuty
+        `0${date.getSeconds()}`.slice(-2)  // Sekundy
+    ];
+    return t.join(':');
 }
 
 class Main {
@@ -29,19 +31,20 @@ class Main {
     constructor() {
         console.log('Starting...');
         console.log('************************************');
-        console.log('*        NekoBot alpha v.0009      *');
-        console.log('* Last changes from day 07.05.2018 *');
+        console.log('*        NekoBot alpha v.0010      *');
+        console.log('* Last changes from day 19.09.2018 *');
         console.log('************************************');
 
         this.client = new Discord.Client();
         this.client.login(new Cfg().getToken('DiscordBot'));
+        this.ModulesLoader = new ModulesLoader(this.client);
 
         this.debugLock = true;
         /* Ładowanie modułów*/
         //this.PlayIt = new VoiceManager();
         this.userWatch = new userWatch(this.client);
         this.trig = new Trigger(this.client);
-        this.trig.loadTriggers();
+        //this.trig.loadTriggers();
         /* Listeners */
         this.initCLI();
         this.initListeners(this.trig);
@@ -87,7 +90,7 @@ class Main {
             /* Czas wiadomości hh:mm:ss */
             var messageTime = time();
             console.log(messageTime + ' Connected!');
-            this.client.user.setActivity('NekoBot alpha v.0009');
+            this.client.user.setActivity('NekoBot alpha v.0010');
         });
         this.client.on('disconnect', closeEvent => {
             //client.destroy();
@@ -114,46 +117,6 @@ class Main {
                 /* Czas wiadomości hh:mm:ss */
                 var messageTime = time();
                 console.log(messageTime + ' debug {' + info + '}');
-            }
-        });
-        // voice listener
-        this.client.on('voiceStateUpdate', (oldMember, newMember) => {
-
-            if (newMember.voiceChannelID === null || newMember.voiceChannelID === undefined) {
-                var guild = newMember.guild;
-            } else {
-                var guild = oldMember.guild;
-            }
-
-            var channelsArray = new Cfg().logsChannels;
-            var logsChannelId = channelsArray[guild.id];
-            var messageTime = time();
-
-            //var guild = this.client.guilds.get(new Cfg().logsChannels);
-
-            if (newMember.voiceChannelID !== oldMember.voiceChannelID && logsChannelId !== null) {
-                var logsChannel = guild.channels.get(logsChannelId);
-
-                if (oldMember.voiceChannelID === null || oldMember.voiceChannelID === undefined) {
-                    logsChannel.send('[' + messageTime + '] **' + newMember.displayName + '** (' + newMember.user.username + ')' + ' joined to **' + newMember.voiceChannel + '**');
-                } else if (newMember.voiceChannelID && oldMember.voiceChannelID) {
-                    logsChannel.send('[' + messageTime + '] **' + newMember.displayName + '** (' + newMember.user.username + ')' + ' switched from **' + oldMember.voiceChannel + '** to **' + newMember.voiceChannel + '**');
-                } else {
-                    logsChannel.send('[' + messageTime + '] **' + oldMember.displayName + '** (' + oldMember.user.username + ')' + ' left **' + oldMember.voiceChannel + '**');
-                }
-
-                //serverDeaf serverMute
-            }
-            if (newMember.guild.id !== oldMember.guild.id) {
-                if (channelsArray[oldMember.guild.id] !== null) {
-                    var ologsChannel = oldMember.guild.channels.get(channelsArray[oldMember.guild.id]);
-                    ologsChannel.send('[' + messageTime + '] **' + oldMember.displayName + '** (' + oldMember.user.username + ')' + ' left **' + oldMember.voiceChannel + '**');
-                }
-
-                if (channelsArray[newMember.guild.id] !== null) {
-                    var nlogsChannel = newMember.guild.channels.get(channelsArray[newMember.guild.id]);
-                    nlogsChannel.send('[' + messageTime + '] **' + newMember.displayName + '** (' + newMember.user.username + ')' + ' joined to **' + newMember.voiceChannel + '**');
-                }
             }
         });
     }
---
+++ modules/VoiceState/State.js
@@ -0,0 +1,55 @@
+const Cfg = new require('../../includes/Config.js');
+
+class voiceState {
+    constructor(client) {
+        client.on('voiceStateUpdate', (oldMember, newMember) => {
+
+            if (newMember.voiceChannelID === null || newMember.voiceChannelID === undefined) {
+                var guild = newMember.guild;
+            } else {
+                var guild = oldMember.guild;
+            }
+
+            var channelsArray = new Cfg().logsChannels;
+            if (channelsArray.hasOwnProperty(guild.id)) {
+                // Czy istnieje w bazie przypisany kanał do logowania statusu
+                var logsChannelId = channelsArray[guild.id];
+
+                let date = new Date();
+                let t = [
+                    `0${date.getHours()}`.slice(-2),   // Godziny
+                    `0${date.getMinutes()}`.slice(-2), // Minuty
+                    `0${date.getSeconds()}`.slice(-2)  // Sekundy
+                ];
+                var messageTime = t.join(':');
+
+                //var guild = this.client.guilds.get(new Cfg().logsChannels);
+
+                if (newMember.voiceChannelID !== oldMember.voiceChannelID && logsChannelId !== null) {
+                    var logsChannel = guild.channels.get(logsChannelId);
+
+                    if (oldMember.voiceChannelID === null || oldMember.voiceChannelID === undefined) {
+                        logsChannel.send('[' + messageTime + '] **' + newMember.displayName + '** (' + newMember.user.username + ')' + ' joined to **' + newMember.voiceChannel + '**');
+                    } else if (newMember.voiceChannelID && oldMember.voiceChannelID) {
+                        logsChannel.send('[' + messageTime + '] **' + newMember.displayName + '** (' + newMember.user.username + ')' + ' switched from **' + oldMember.voiceChannel + '** to **' + newMember.voiceChannel + '**');
+                    } else {
+                        logsChannel.send('[' + messageTime + '] **' + oldMember.displayName + '** (' + oldMember.user.username + ')' + ' left **' + oldMember.voiceChannel + '**');
+                    }
+
+                    //serverDeaf serverMute
+                }
+                if (newMember.guild.id !== oldMember.guild.id) {
+                    if (channelsArray[oldMember.guild.id] !== null) {
+                        var ologsChannel = oldMember.guild.channels.get(channelsArray[oldMember.guild.id]);
+                        ologsChannel.send('[' + messageTime + '] **' + oldMember.displayName + '** (' + oldMember.user.username + ')' + ' left **' + oldMember.voiceChannel + '**');
+                    }
+
+                    if (channelsArray[newMember.guild.id] !== null) {
+                        var nlogsChannel = newMember.guild.channels.get(channelsArray[newMember.guild.id]);
+                        nlogsChannel.send('[' + messageTime + '] **' + newMember.displayName + '** (' + newMember.user.username + ')' + ' joined to **' + newMember.voiceChannel + '**');
+                    }
+                }
+            }
+        });
+    }
+}
