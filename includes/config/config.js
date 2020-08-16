360f4bf4efc93257fb4bf1e8701f5e8d697d0716
Kamil Ś <Kamaz91@users.noreply.github.com>
2020-08-16 18:25:57 +0200

    rebuilded config to class block added notifier settings

--- includes/config/config.js
+++ includes/config/config.js
@@ -6,75 +6,106 @@ const db = require('../database/index.js');
 
 var Emitter = new events.EventEmitter();
 
-var config = {
-    // Discord.js extended Map Collection
-    guilds: new Discord.Collection()
-};
+class Config {
+    constructor() {
+        this.guilds = null;
+        this.TemplateGuilds = null;
+        this.configJob = new CronJob('0 */5 * * * *', async () => {
+            console.log('Synchronization with database');
+            await this.loadGuildConfig();
+            Emitter.emit('config-updated');
+        });
 
-const configJob = new CronJob('0 */10 * * * *', function () {
-    console.log('Synchronization with database');
-    Emitter.emit('config-updated');
-});
-
-client.once('ready', async () => {
-    configJob.start();
-    await loadConfig();
-    Emitter.emit('config-ready');
-});
-
-async function loadConfig() {
-    var TemplateGuilds = new Object;
-    // GET ids of all connected guilds
-    var guildsKeys = client.guilds.keyArray();
-    for (const guild_id of guildsKeys) {
-        TemplateGuilds[guild_id] = settingsTemplate(guild_id);
+        client.once('ready', async () => {
+            this.configJob.start();
+            await this.loadGuildConfig();
+            Emitter.emit('config-ready');
+        });
     }
 
-    var modules = await db.guildManagment.settings.modules.getBulkByid(guildsKeys);
-    if (modules.status)
-        for (const el of modules.request) {
-            TemplateGuilds[el.guild_id].modules[el.module_name].enabled = el.enabled ? true : false
-        }
+    async loadGuildConfig() {
+        // GET ids of all connected guilds
+        var guildsKeys = client.guilds.keyArray();
+        this.TemplateGuilds = await this.loadModulesSettings(guildsKeys);
+        await this.loadAutoPurge(guildsKeys);
+        await this.loadNotifier(guildsKeys);
 
-    var autoPurge = await db.guildManagment.settings.autoPurge.getBulkByid(guildsKeys);
-
-    if (autoPurge.status)
-        for (const el of autoPurge.request) {
-            TemplateGuilds[el.guild_id].modules.autoPurge.channels.set(el.channel_id, { id: el.channel_id, time: el.older_than });
-        }
-
-    for (const [key, template] of Object.entries(TemplateGuilds)) {
-        config.guilds.set(key.toString(), template);
+        this.saveGuildsSettings();
     }
-}
 
-function updateConfig() {
+    saveGuildsSettings() {
+        var guildsCollection = new Discord.Collection();
+        for (const [key, template] of Object.entries(this.TemplateGuilds)) {
+            guildsCollection.set(key.toString(), template);
+        }
+        this.TemplateGuilds = null;
+        this.guilds = guildsCollection;
+    }
 
-}
+    settingsTemplate(guildsKeys) {
+        var template = new Object;
+        for (const guild_id of guildsKeys) {
+            template[guild_id] = {
+                id: guild_id,
+                prefix: "!",
+                modules: {
+                    autoPurge: {
+                        enabled: false,
+                        channels: new Discord.Collection()
+                    },
+                    messageCounter: {
+                        enabled: false,
+                        data: null
+                    },
+                    quotes: {
+                        enabled: false
+                    },
+                    logsChannels: {
+                        enabled: false,
+                        action: null,
+                        voice: null
+                    },
+                    notifier: {
+                        enabled: false,
+                        memberleft: {
+                            users: new Discord.Collection()
+                        }
+                    }
+                }
+            };
+        }
+        return template;
+    }
 
-function settingsTemplate(guild_id) {
-    return {
-        id: guild_id,
-        prefix: "!",
-        modules: {
-            autoPurge: {
-                enabled: false,
-                channels: new Discord.Collection()
-            },
-            messageCounter: {
-                enabled: false,
-                data: null
-            },
-            quotes: {
-                enabled: false
-            },
-            logsChannels: {
-                enabled: false,
-                action: null,
-                voice: null
+    async loadModulesSettings(guildsKeys) {
+        var template = this.settingsTemplate(guildsKeys);
+        var modules = await db.guildManagment.settings.modules.getBulkByid(guildsKeys);
+        if (modules.status)
+            for (const el of modules.request) {
+                template[el.guild_id].modules[el.module_name].enabled = el.enabled ? true : false
             }
-        }
-    };
+        return template;
+    }
+
+    async loadAutoPurge(guildsKeys) {
+        var autoPurge = await db.guildManagment.settings.autoPurge.getBulkByid(guildsKeys);
+
+        if (autoPurge.status)
+            for (const el of autoPurge.request) {
+                this.TemplateGuilds[el.guild_id].modules.autoPurge.channels.set(el.channel_id, { id: el.channel_id, time: el.older_than });
+            }
+    }
+
+    async loadNotifier(guildsKeys) {
+        var notifier = await db.guildManagment.settings.notifier.getBulkByid(guildsKeys);
+
+        if (notifier.status)
+            for (const el of notifier.request) {
+                this.TemplateGuilds[el.guild_id].modules.notifier[el.type].users.set(el.user_id, { id: el.user_id });
+            }
+    }
 }
+
+var config = new Config();
 
 module.exports = { config: config, events: Emitter };
