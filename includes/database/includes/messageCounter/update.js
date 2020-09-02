e4a7a9ee188438f52db85f9ce952ac305e4229fb
Kamil Ś <Kamaz91@users.noreply.github.com>
2020-09-02 04:48:03 +0200

    added messageCounter db helpers to increment user stats and guild

--- includes/database/includes/messageCounter/update.js
+++ includes/database/includes/messageCounter/update.js
@@ -21,7 +21,52 @@ module.exports = (Connection) => {
                 })
                 .then((rows) => {
                     if (rows > 0) {
-                        return { status: true, error: false, request: rows[0] };
+                        return { status: true, error: false, request: rows };
+                    } else {
+                        return { status: false, error: false, request: "No rows updated" }
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                    return { status: false, error: true, request: err }
+                });
+        },
+        incrementUserStats(user_id, guild_id, words, chars, attachments) {
+            return Connection('message_counter_user_stats')
+                .where({
+                    user_id: user_id,
+                    guild_id: guild_id
+                })
+                .increment({
+                    total_messages: 1,
+                    total_words: words,
+                    total_chars: chars,
+                    total_attachments: attachments
+                })
+                .update({ last_message_timestamp: moment().valueOf() })
+                .then((rows) => {
+                    if (rows > 0) {
+                        return { status: true, error: false, request: rows };
+                    } else {
+                        return { status: false, error: false, request: "No rows updated" }
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                    return { status: false, error: true, request: err }
+                });
+        },
+        incrementGuildCurrentHour(guild_id) {
+            let hour = moment().format("H");
+            return Connection('message_counter_guilds')
+                .where({
+                    guild_id: guild_id,
+                    ymd: moment().format("YYYYMMDD")
+                })
+                .increment(hour, 1)
+                .then((rows) => {
+                    if (rows > 0) {
+                        return { status: true, error: false, request: rows };
                     } else {
                         return { status: false, error: false, request: "No rows updated" }
                     }
