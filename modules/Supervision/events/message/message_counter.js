f324a91d83a41da29f461903b9b01f51f0747a40
Kamil Ś <Kamaz91@users.noreply.github.com>
2020-09-02 05:32:32 +0200

    Merged messageConter into one file changed to new config added white/black/bot listing

--- modules/Supervision/events/message/message_counter.js
+++ modules/Supervision/events/message/message_counter.js
@@ -1,38 +1,70 @@
-const moment = require('moment');
+const db = require('../../../../includes/database/index.js');
+const { config } = require('../../../../includes/config/config.js');
 
 class message_counter {
     constructor(args) {
-        this.message = args[0];
-        try {
-            if (this.message.channel.type == "text" &&
-                !this.message.author.bot &&
-                this.message.guild
-            ) {
-                var userid = this.message.author.id;
-                var guildid = this.message.guild.id;
-                var ymd = moment().format("YYYYMMDD");
-                var hour = moment().format("H");
+        var message = args[0];
 
-                knex('message_counter')
-                    .where({ user_id: userid, guild_id: guildid, ymd: ymd })
-                    .increment(hour, 1)
-                    .then(i => {
-                        // if 0 db cant find row to update so create one
-                        if (i === 0) {
-                            knex('message_counter').insert({
-                                user_id: userid,
-                                guild_id: guildid,
-                                ymd: ymd,
-                                [hour]: 1
-                            })
-                                .then()
-                                .catch(console.error);
-                        }
-                    })
-                    .catch(console.error);
+        if (message.channel.type == "text" && message.guild) {
+
+            var user_id = message.author.id;
+            var guild_id = message.guild.id;
+            var channel_id = message.channel.id;
+            // TODO Listing kanałów MC blacklist/whitelist
+
+            var guild = config.guilds.get(guild_id);
+            if (!guild.modules.messageCounter.enabled) {
+                return;
             }
-        } catch (e) {
-            console.error(e);
+
+            if (
+                (
+                    message.author.bot && !(message.author.bot && guild.modules.messageCounter.bots == true)
+                ) ||
+                (
+                    (
+                        guild.modules.messageCounter.listing.type == "whitelist" &&
+                        !guild.modules.messageCounter.listing.channels.get(channel_id)
+                    ) ||
+                    (
+                        guild.modules.messageCounter.listing.type == "blacklist" &&
+                        guild.modules.messageCounter.listing.channels.get(channel_id)
+                    )
+                )
+            ) {
+                return;
+            }
+
+            var words = message.content.split(' ').length;
+            var chars = message.content.length;
+            var attachments = 0;
+
+            // attachments counter 
+            if (message.attachments.array().length > 0) {
+                attachments = message.attachments.array().length;
+            }
+
+            // User Hour
+            db.messageCounter.update.incrementUserCurrentHour(user_id, guild_id)
+                .then((result) => {
+                    if (!result.status) {
+                        db.messageCounter.insert.UserDay(user_id, guild_id);
+                    }
+                });
+            // User Stats
+            db.messageCounter.update.incrementUserStats(user_id, guild_id, words, chars, attachments)
+                .then((result) => {
+                    if (!result.status) {
+                        db.messageCounter.insert.UserStats(user_id, guild_id, words, chars, attachments);
+                    }
+                });
+            // Guild Hour
+            db.messageCounter.update.incrementGuildCurrentHour(guild_id)
+                .then((result) => {
+                    if (!result.status) {
+                        db.messageCounter.insert.GuildDay(guild_id);
+                    }
+                });
         }
     }
 }
