30409c77f8d19071a98255f351c190e9afe216c3
Kamil Ś <Kamaz91@users.noreply.github.com>
2020-09-02 05:27:56 +0200

    updated to discord.js v12 changed tp new config

--- modules/Supervision/events/voiceStateUpdate/voiceChannelNotifier.js
+++ modules/Supervision/events/voiceStateUpdate/voiceChannelNotifier.js
@@ -1,51 +1,53 @@
-const Cfg = require('../../../../includes/Config.js');
-const { MessageEmbed } = require('discord.js'); // for embed builder
-var config = new Cfg();
+const { MessageEmbed } = require('discord.js'); // embed builder
+const { config } = require('../../../../includes/config/config.js');
 
 class voiceChannelNotifier {
     constructor(args) {
-        var oldMember = args[0];
-        var newMember = args[1];
+        // VoiceState variables new in discord.js v12
+        // https://discord.js.org/#/docs/main/v12/class/Client?scrollTo=e-voiceStateUpdate
+        var oldState = args[0];
+        var newState = args[1];
 
-        try {
-            if (newMember.voiceChannelID === null || newMember.voiceChannelID === undefined) {
-                var guild = newMember.guild;
-            } else {
-                var guild = oldMember.guild;
-            }
+        // Get guild data 
+        var guild = newState.guild;
 
-            if (config.logsChannels.hasOwnProperty(guild.id)) {
-                // Jeśli istnieje przypisany kanał do logów
-                var logsChannelId = config.logsChannels[guild.id];
-                if (newMember.channelID !== oldMember.channelID && logsChannelId !== null) {
-                    var logsChannel = guild.channels.resolve(logsChannelId);
-                    if (oldMember.channelID === null || oldMember.channelID === undefined) {
-                        var author = `${newMember.member.displayName}#${newMember.member.user.discriminator}`;
-                        var img = newMember.member.user.displayAvatarURL;
-                        var action = 'Joined to **' + newMember.channel.name + '**';
-                        var color = [79, 214, 38];
-                    } else if (newMember.channelID && oldMember.channelID) {
-                        var author = `${newMember.member.displayName}#${newMember.member.user.discriminator}`;
-                        var img = newMember.member.user.displayAvatarURL;
-                        var action = 'Switched from **' + oldMember.channel.name + '** to **' + newMember.channel.name + '**';
-                        var color = [33, 108, 198];
-                    } else {
-                        var author = `${oldMember.member.displayName}#${oldMember.member.user.discriminator}`;
-                        var img = oldMember.member.user.displayAvatarURL;
-                        var action = `Left **${oldMember.channel.name}**`;
-                        var color = [214, 44, 38];
-                    }
+        // Get guild config data 
+        var guildConfig = config.guilds.get(guild.id);
 
-                    const embed = new MessageEmbed()
-                        .setTimestamp()
-                        .setAuthor(author, img)
-                        .setDescription(action)
-                        .setColor(color)
-                    logsChannel.send({ embed: embed });
+        // Is enabled logs for guild
+        if (guildConfig.modules.logsChannels.enabled) {
+            var logsChannel = guild.channels.resolve(guildConfig.modules.logsChannels.voice);
+            // Check if the log channel exists in config
+            if (newState.channelID !== oldState.channelID && logsChannel != null) {
+
+                if (oldState.channelID === null) {
+                    //* Joined to voice channel
+                    var author = `${newState.member.displayName}#${newState.member.user.discriminator}`;
+                    var img = newState.member.user.displayAvatarURL;
+                    var action = 'Joined to **' + newState.channel.name + '**';
+                    var color = [79, 214, 38];
+                } else if (newState.channelID && oldState.channelID) {
+                    //* Switched voice channels
+                    var author = `${newState.member.displayName}#${newState.member.user.discriminator}`;
+                    var img = newState.member.user.displayAvatarURL;
+                    var action = 'Switched from **' + oldState.channel.name + '** to **' + newState.channel.name + '**';
+                    var color = [33, 108, 198];
+                } else {
+                    //* Left voice channel
+                    var author = `${oldState.member.displayName}#${oldState.member.user.discriminator}`;
+                    var img = oldState.member.user.displayAvatarURL;
+                    var action = `Left **${oldState.channel.name}**`;
+                    var color = [214, 44, 38];
                 }
+
+                const embed = new MessageEmbed()
+                    .setTimestamp()
+                    .setAuthor(author, img)
+                    .setDescription(action)
+                    .setColor(color)
+
+                logsChannel.send(embed);
             }
-        } catch (e) {
-            console.log(e);
         }
     }
 }
