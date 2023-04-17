a079759908b97504fde252d8392446754e214779
Kamil Ś <Kamaz91@users.noreply.github.com>
2023-04-17 12:14:31 +0200

    Create EnabledModules.ts

---
+++ src/EnabledModules.ts
@@ -0,0 +1,11 @@
+import AutoPurge from "@modules/AutoPurge"
+import MessageCounter from "@modules/MessageCounter"
+import Quotes from "@modules/Quotes"
+import Notifier from "@modules/Notifier"
+
+export default {
+    AutoPurge,
+    MessageCounter,
+    Notifier,
+    Quotes
+}
