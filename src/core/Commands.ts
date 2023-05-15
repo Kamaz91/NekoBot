42444cdc47e6173760f3ddf8d28dc2f4efda8c07
Kamil Ś <Kamaz91@users.noreply.github.com>
2023-05-15 14:44:31 +0200

    Delete Commands.ts

--- src/core/Commands.ts
+++
@@ -1,6 +0,0 @@
-import * as commands from "@src/EnabledCommands"
-import { InteractionManager } from "@core/Bot"
-
-for (let [, command] of Object.entries(commands)) {
-    InteractionManager.addGlobalInteraction(command.name, command.execute);
-}
