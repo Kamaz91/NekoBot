925822fbb3b0f54eed64450091e76fa2dd00fe88
Kamaz91 <Kamaz91@gmail.com>
2025-03-10 12:25:16 +0100

    added log node version

--- src/NekoBot.ts
+++ src/NekoBot.ts
@@ -2,6 +2,7 @@ import "./core/env.js";
 import logger from './services/logger/index.js';
 
 logger.info('Starting...');
+logger.info(`Node.js version: ${process.version}`);
 logger.info('*****************************');
 logger.info('*      NekoBot v13.0.0      *');
 logger.info('*****************************');
